9b33c585818fcdeeff428bd0f58abce6af5ba5b8
tomaskls <tomaskls@hotmail.com>
2024-07-29 13:36:36 +0300

    edit

--- js/kintamieji/object.js
+++ js/kintamieji/object.js
@@ -29,12 +29,12 @@ const items = [
     },
     {
         title:'sviestas',
-        price: 2.2,
+        price: 2,
         units: 2,
     },
     {
         title:'pienas',
-        price: 0.5,
+        price: 5,
         units: 3,
     },
     {
@@ -44,42 +44,39 @@ const items = [
     },
     {
         title:'piestukas',
-        price: 1.4,
+        price: 1,
         units: 14,
     },
-    {
-        title:'agurkas',
-        price: 0.8,
-        units: 9,
-    },
+ 
 ]
-const items1 = items[0];
-const line1 = items1.title + ' ' + items1.price +
-' Eur ' + '(kaina uz vnt) ' + 'Kiekis - ' + items1.units;
+const item1 = items[0];
+const line1 = item1.title + ' ' + item1.price +
+' Eur ' + '(kaina uz vnt) ' + 'Kiekis - ' + item1.units;
 
-const items2 = items[1];
-const line2 = items2.title + ' ' + items2.price +
-' Eur ' + '(kaina uz vnt) ' + 'Kiekis - ' + items2.units;
+const item2 = items[1];
+const line2 = item2.title + ' ' + item2.price +
+' Eur ' + '(kaina uz vnt) ' + 'Kiekis - ' + item2.units;
 
-const items3 = items[2];
-const line3 = items3.title + ' ' + items3.price +
-' Eur ' + '(kaina uz vnt) ' + 'Kiekis - ' + items3.units;
+const item3 = items[2];
+const line3 = item3.title + ' ' + item3.price +
+' Eur ' + '(kaina uz vnt) ' + 'Kiekis - ' + item3.units;
 
-const items4 = items[3];
-const line4 = items4.title + ' ' + items4.price +
-' Eur ' + '(kaina uz vnt) ' + 'Kiekis - ' + items4.units;
+const item4 = items[3];
+const line4 = item4.title + ' ' + item4.price +
+' Eur ' + '(kaina uz vnt) ' + 'Kiekis - ' + item4.units;
 
-const items5 = items[4];
-const line5 = items5.title + ' ' + items5.price +
-' Eur ' + '(kaina uz vnt) ' + 'Kiekis - ' + items5.units;
+const item5 = items[4];
+const line5 = item5.title + ' ' + item5.price +
+' Eur ' + '(kaina uz vnt) ' + 'Kiekis - ' + item5.units;
 
-let count=items1.units + items2.units + items3.units + items4.units
-+ items5.units 
-let totalPrice=items1.price * items1.units +
-items2.price * items2.units +
-items3.price * items3.units +
-items4.price * items4.units +
-items5.price * items5.units 
+let count=item1.units + item2.units + item3.units + item4.units
++ item5.units; 
+
+let totalPrice= item1.price * item1.units +
+item2.price * item2.units +
+item3.price * item3.units +
+item4.price * item4.units +
+item5.price * item5.units;
 
 console.log('Prekiu krepselyje yra ' + count + ' vnt prekiu');
 console.log('---------------------------------');
